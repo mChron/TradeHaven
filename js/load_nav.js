2a6a058b4b9ac526c15a6eb4d529bc8c2a16efa2
Marcus Chronabery <mChron@users.noreply.github.com>
2018-09-24 14:28:02 -0400

    Added ajax methods to load header/footer/modals

--- js/load_nav.js
+++ js/load_nav.js
@@ -6,8 +6,12 @@ $(function() {
     if (isChrome) {
         loadHeaderAndFooter();
         loadCommonModal("Login", "Login", $("link[rel='import'][href='pages/login_modal_content.html']")[0].import.querySelector('div'));
-        loadCopyrightYear();
     }
+    else {
+        loadHeaderAndFooterAjax();
+        loadCommonModalAjax("Login", "Login", "pages/login_modal_content.html");
+    }
+    loadCopyrightYear();
 });
 
 /*
@@ -33,13 +37,74 @@ function loadHeaderAndFooter() {
         }
         document.body.appendChild(htmlMessage.cloneNode(true));
     });
-    
+    setActiveNavLink();
+}
+
+/*Load header and footer via ajax*/
+function loadHeaderAndFooterAjax() {
+    $.ajax({
+        dataType:"html",
+        url: "pages/header.html",
+        success: function(data) {
+            var head = $(data)[3];
+            $(document.body).append($(head).clone());
+        }
+    });
+    $.ajax({
+        dataType:"html",
+        url: "pages/footer.html",
+        success: function(data) {
+            var foot = $(data)[3];
+            $(document.body).append($(foot).clone());
+        }
+    });
+    setActiveNavLink();
+}
+
+/*Set the current page link to 'active' in the navigation bar*/
+function setActiveNavLink() {
     // get the current url and set the appropriate nav-link as active
     var url = document.location.href;
     var last = url.lastIndexOf("/");
     last = url.substring(last+1);
     $(".nav-link[href='" + last + "']").addClass("active");
     $(".nav-link[href='pages/" + last + "']").addClass("active");
+}
+
+/*Load common modal with content from the provided url via ajax */
+function loadCommonModalAjax(modalTitle, mainBtnText, url) {
+    $.ajax({
+        dataType:"html",
+        url: "pages/common_centered_modal.html",
+        success: function(modalData) {
+            $.ajax({
+                dataType:"html",
+                url: url,
+                success: function(data) {
+                    var content = $(modalData)[3];
+                    var prefix = modalTitle.toString().toLowerCase() + "-";
+                    var modal = $(content);
+
+                    // set modal element ids to reflect which modal they belong to
+                    modal.prop("id", prefix + "modal");
+                    $(modal).find(".modal-content").prop("id", prefix + "modal-content");
+                    $(modal).find(".modal-body").prop("id", prefix + "modal-body");
+                    $(modal).find("#" + prefix + "modal-body").html($(data)[3]);
+                    $(modal).find(".modal-header").prop("id", prefix + "modal-header");
+                    $(modal).find(".modal-title").html(modalTitle);
+
+                    // modify the submit button on the modal to reflect it's purpose
+                    var loginSubmitBtn = $(modal).find(".modal-footer button.btn-primary");
+                    $(loginSubmitBtn).html(mainBtnText);
+                    $(loginSubmitBtn).prop("id", prefix + "submit");
+                    $(loginSubmitBtn).click(function() {
+                        $("#login-form").submit();
+                    });
+                    $(document.body).append($(content).clone());
+                }
+            });
+        }
+    });
 }
 
 /*
