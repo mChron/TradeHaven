86ac7eb7d4b83ce9ac3a5f5fd4d6de3151b99bce
Marcus Chronabery <mChron@users.noreply.github.com>
2018-10-09 10:33:00 -0400

    Formatted events js...
    Formatted for spacing in events.js and added additional listener for the date picker input to ensure dates in the past are not able to be specified.

--- js/events.js
+++ js/events.js
@@ -4,18 +4,41 @@
  * date: 10/08/18
  */
 $(function() {
-   $("#event-privacy-btn").click(function() {
-       if ($(this).html() === "Private (Invite Only)") {
-           $(this).html("Public");
-       }
-       else {
-           $(this).html("Private (Invite Only)");
-       }
-       $(this).blur();
-   }) ;
-   $("#create-event-form").submit(validateEventForm);
+	$("#event-privacy-btn").click(function() {
+		if ($(this).html() === "Private (Invite Only)") {
+		    $(this).html("Public");
+		}
+		else {
+		    $(this).html("Private (Invite Only)");
+		}
+		$(this).blur();
+	}) ;
+	$("#create-event-form").submit(validateEventForm);
+	$("#event-date-input").on("change", function() {
+		var today = new Date();
+		var val = new Date($(this).val());
+		var invalid;
+		if(val.toString() === "Invalid Date") {
+			val = today;
+			invalid = true;
+		}
+		var sameYear = today.getYear() === val.getYear();
+		var sameMonth = today.getMonth() === val.getMonth();
+		var prevYear = val.getYear() < today.getYear();
+		var prevMonth = val.getMonth() < today.getMonth();
+		var prevDay = val.getDate() < today.getDate();
+		if (invalid || prevYear || (sameYear && prevMonth) || (sameYear && sameMonth && prevDay)) {
+			var newVal = ("0" + (today.getMonth()+1)).slice(-2) + "/"
+				+ ("0" + today.getDate()).slice(-2) + "/"
+				+ today.getFullYear();
+			$(this).val(newVal);
+		}
+	});
 });
 
+/*
+ * Validate the events form for required attributes.
+ */
 function validateEventForm(e) {
     e.preventDefault();
     toggleWarning("#event-name", !$("#event-name-input").val());
