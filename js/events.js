401747a07e298a6fffb8bcf8459f3ecab2ebfe2e
iI9RmxRgyg <marcuschron@gmail.com>
2018-10-09 14:58:39 -0400

    Fixed date validation for events. Added additional methods for formatting dates.

--- js/events.js
+++ js/events.js
@@ -4,40 +4,85 @@
  * date: 10/08/18
  */
 $(function() {
-	$("#event-privacy-btn").click(function() {
-		if ($(this).html() === "Private (Invite Only)") {
-		    $(this).html("Public");
-		}
-		else {
-		    $(this).html("Private (Invite Only)");
-		}
-		$(this).blur();
-	}) ;
-	$("#create-event-form").submit(validateEventForm);
-	$("#event-date-input").on("change", function() {
-		var today = new Date();
-		var val = new Date($(this).val());
-		var invalid;
-		if(val.toString() === "Invalid Date") {
-			val = today;
-			invalid = true;
-		}
-		var sameYear = today.getYear() === val.getYear();
-		var sameMonth = today.getMonth() === val.getMonth();
-		var prevYear = val.getYear() < today.getYear();
-		var prevMonth = val.getMonth() < today.getMonth();
-		var prevDay = val.getDate() < today.getDate();
-		if (invalid || prevYear || (sameYear && prevMonth) || (sameYear && sameMonth && prevDay)) {
-			var newVal = ("0" + (today.getMonth()+1)).slice(-2) + "/"
-				+ ("0" + today.getDate()).slice(-2) + "/"
-				+ today.getFullYear();
-			$(this).val(newVal);
-		}
-	});
+    $("#event-privacy-btn").click(function() {
+        if ($(this).html() === "Private (Invite Only)") {
+            $(this).html("Public");
+        }
+        else {
+            $(this).html("Private (Invite Only)");
+        }
+        $(this).blur();
+    }) ;
+    $("#create-event-form").submit(validateEventForm);
+    $("#event-date-input").change(validateDateInput);
 });
+
+/**
+ * Validates the input for the date field. If date entered is prior to today 
+ * the value will default to today. If the valid is today or a future value 
+ * but is in a form other than mm/dd/yyyy, the value will be formatted to conform.
+ */
+function validateDateInput() {
+    var today = new Date();
+    var val = new Date($(this).val());
+    var invalid;
+    if(val.toString() === "Invalid Date") {
+        val = today;
+        invalid = true;
+    }
+    if (invalid || val < today) {
+        var newVal = formatDateString(today);
+        $(this).val(newVal);
+    }
+    else {
+        $(this).val(formatDateString(val));
+    }
+}
+
+/**
+ * Return a formatted string from the provided date. If the object is not a
+ * valid date, today's date is used.
+ * @param {type} date The date to format
+ * @returns {String} A formatted date with the form mm/dd/yyyy
+ */
+function formatDateString(date) {
+    if (date === undefined || !(date instanceof Date)) {
+        date = new Date();
+    }
+    var mm = prependZerosLengthN(date.getMonth() + 1, 2);
+    var dd = prependZerosLengthN(date.getDate(), 2);
+    var yyyy = date.getFullYear();
+    return mm + "/" + dd + "/" + yyyy;
+}
+
+/**
+ * Prepends the provided string with zeros up to length n.
+ * If the provided string is already at/beyond length n, you will just get the 
+ * last n characters of the provided string.
+ * @param {type} str The string to prepend with zeros
+ * @param {type} n The desired length of string to return
+ * @returns {String} The provided string prepended with zeros, or the last n
+ * characters of the provided string if the length was beyond n.
+ */
+function prependZerosLengthN(str, n) {
+    var sliceIndx = 0 - n;
+    if (str === undefined) {
+        throw "Cannot format provided argument. Not a string.";
+    }
+    else if (str.length < n) {
+        while (str.length < n) {
+            str = "0" + str;
+        }
+    }
+    return str.toString().slice(sliceIndx);
+}
 
 /*
  * Validate the events form for required attributes.
+ */
+/**
+ * Validate the events form for required attributes.
+ * @param {type} e The triggered event, should be the submit of the form.
  */
 function validateEventForm(e) {
     e.preventDefault();
